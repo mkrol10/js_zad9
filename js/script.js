1af43c17f4baa5a516babbf4deb286e8a4154641
Mateusz Król <m_krol@poczta.wwsi.edu.pl>
2017-02-01 21:32:18 +0100

    remove alerts

--- js/script.js
+++ js/script.js
@@ -2,10 +2,8 @@ var buttonsGet = document.getElementsByClassName('button')
 
 for (i = 0; i < buttonsGet.length; i++) {
     show();
-
 }
 
 function show() {
-    alert(buttonsGet[i].innerText);
     console.log(buttonsGet[i].innerText);
 }
